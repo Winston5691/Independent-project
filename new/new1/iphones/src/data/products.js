83166bb19bd78c1b2dfb2535de2dfe6bbd59f8a5
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-07 20:00:46 +0300

    data

---
+++ new/new1/iphones/src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+      id: 1,
+      name: 'Iphone X',
+      price: 68000,
+      currency: 'Ksh',
+      image: 'images/01.jpg',
+    },
+    {
+      id: 2,
+      name: 'Iphone 10',
+      price: 86000,
+      currency: 'Ksh',
+      image: 'images/02.jpg',
+    },
+    {
+      id: 4,
+      name: 'Iphone 11',
+      price: 110000,
+      currency: 'Ksh',
+      image: 'images/04.jpg',
+    },
+    {
+      id: 6,
+      name: 'Iphone 11 pro',
+      price: 132000,
+      currency: 'Ksh',
+      image: 'images/06.jpg',
+    },
+    {
+      id: 5,
+      name: 'Iphone 12',
+      price: 144000,
+      currency: 'Ksh',
+      image: 'images/05.jpg',
+    },
+    {
+      id: 3,
+      name: 'Iphone 14',
+      price: 220000,
+      currency: 'Ksh',
+      image: 'images/03.jpg',
+    }
+  ];
+  
+  export default data;
