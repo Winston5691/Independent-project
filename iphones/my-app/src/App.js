9113bc10a53dd43b7d477682e46717db9c1c2dcf
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-10 21:45:55 +0300

    Final project done

--- iphones/my-app/src/App.js
+++ iphones/my-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Phones from './Phones';
 import Categories from './Categories'
 
-
+// App
 function App() {
 
     const [phoneItems, setPhoneItems] = useState([]);
